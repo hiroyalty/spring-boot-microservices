9b537dc557847a763e93986c3f4ddfdc14722a6c
Gbolaga Famodun <gbolaga.famodun@gmail.com>
2024-10-18 06:02:35 -0400

    eureka discovery server setup

---
+++ discovery/src/test/java/com/hiroyalty/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiroyalty.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
